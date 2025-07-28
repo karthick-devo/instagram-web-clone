23bc526d3c5e2ac9bf3371bea64e900cb6035505
Karthick <karthickmuthuramann@gmail.com>
2025-07-28 14:51:58 +0530

    homenavigation

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react'
 import "../Login/index.css"
-
-
-
-
-
+import {useNavigate} from 'react-router'
 
 
 function Login() {
-const [password,setPassword] = useState("");
-const [error , setError] = useState('');
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
-const handleSubmit = (e)=>{
-  e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  if (password.length< 8){
-    setError("Password must be 8 characters")
-  } else{
-    setError('');
-    console.log("Logging in...");
-    
+    if (password.length < 8) {
+      setError("Password must be 8 characters")
+    } else {
+      setError('');
+      console.log("Logging in...");
+      navigate('/home')
+
+    }
   }
-}
   return (
     <div className='login-container d-flex flex-row' >
       <div>
@@ -31,13 +29,13 @@ const handleSubmit = (e)=>{
         <h1 className='logo'>Instagram</h1>
         <form className="login-form" onSubmit={handleSubmit}>
           <input type="text" placeholder="Phone number, username, or email" />
-          <input type="password" placeholder="Password" value={password} onChange={(e)=> setPassword(e.target.value)} className='password'/>
-           {error && <p className="error-message">{error}</p>}
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit">Log In</button>
         </form>
         <div className="divider">OR</div>
 
         <button className="facebook-login">Log in with Facebook</button>
+        {error && <p className="error-message">{error}</p>}
         <a href="#" className="forgot-password">Forgot password?</a>
         <div className="signup-box">
           Don't have an account? <a href="#">Sign up</a>
